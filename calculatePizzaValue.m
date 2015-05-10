f07f7a305c38dd402832edc386f8b1e3d4344128
David Bradway <david.bradway@gmail.com>
2015-05-10 13:41:28 -0400

    remove empty functions

--- calculatePizzaValue.m
+++
@@ -1,7 +0,0 @@
-function [costPerUnitArea] = calculatePizzaValue(diameter,price)
-% This function takes a pizza diameter and price as inputs
-% and returns the cost per unit area
-    
-    % Error check the inputs
-end
-
--- lineLengthAndSlope.m
+++
@@ -1,7 +0,0 @@
-function [len,slope] = lineLengthAndSlope(x0,x1,y0,y1)
-% This function takes x and y coordinates of a line as inputs
-% and returns the length and slope of the line as outputs.
-    
-    % Error check the inputs
-end
-
